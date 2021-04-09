3fe63c3a7176f1526a059fa1094505aec4578ff0
tony <idokoanthony86@yahoo.com>
2021-04-09 06:08:44 +0100

    changed property from display to visibility

--- main.js
+++ main.js
@@ -3,15 +3,13 @@ var toggleIcon = true;
 
  function displayContent(){
     let getulLink = document.querySelector("ul.nav-links");
-    let getulLinkLi = document.querySelector("ul.nav-links li");
     if (toggleIcon) {
-        getulLink.style.visibility = 'visible';
+        getulLink.style.visibility = "visible";
         toggleIcon = false; 
     }
     
     else{
         getulLink.style.visibility = "hidden";
-
         toggleIcon = true;    
     }   
 };
