df5613cc8f74dc5fe520b8439eee381a7997e616
tony <idokoanthony86@yahoo.com>
2021-04-09 06:23:12 +0100

    commented the JS file slightly

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 var toggleIcon = true;
 
  function displayContent(){
-    let getulLink = document.querySelector("ul.nav-links");
+    let getulLink = document.querySelector("ul.nav-links"); //target the section holding nav links and store in the variable
     if (toggleIcon) {
-        getulLink.style.visibility = "visible";
+        getulLink.style.visibility = "visible"; // Set property to visible from hidden
         toggleIcon = false; 
     }
     
